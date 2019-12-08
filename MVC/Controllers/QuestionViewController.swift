c2481b4dd8cd5a0a9777b534763679eca35496a1
victor <infragbe@gmail.com>
2019-12-08 13:02:08 +0900

    Project Re-organization

--- MVC/Controllers/QuestionViewController.swift
+++ MVC/Controllers/QuestionViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class QuestionViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override public func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
