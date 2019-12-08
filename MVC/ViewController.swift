e54eca87e6d4c35a8dfca4cced38fb83caac7aec
victor <infragbe@gmail.com>
2019-12-08 12:38:33 +0900

    Initial Commit

---
+++ MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by victor on 08/12/2019.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
