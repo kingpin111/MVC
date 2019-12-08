b443bb7c60371772dcf2f6084cbc639fa9304724
victor <infragbe@gmail.com>
2019-12-08 16:11:13 +0900

    Connet QuestionView and QuestionViewController

--- MVC/Controllers/QuestionViewController.swift
+++ MVC/Controllers/QuestionViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 public class QuestionViewController: UIViewController {
-
-    override public func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // Mark: - Instance Properties
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
+    
+    public var correctCount = 0
+    public var incorrectCount = 0
+    
+    public var questionView: QuestionView! {
+        guard isViewLoaded else { return nil }
+        return (view as! QuestionView)
     }
 
+    // MARK: - View Lifecycle
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        showQuestion()
+    }
+    
+    fileprivate func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+        
+        questionView.answerLabel.text = question.answer
+        questionView.promptLabel.text = question.prompt
+        questionView.hintLabel.text = question.hint
+        
+        questionView.answerLabel.isHidden = true
+        questionView.hintLabel.isHidden = true
+    }
+    
 
 }
 
