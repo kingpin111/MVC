08a8649bb910ed3bfa4d113cc5f27fff420f516f
victor <infragbe@gmail.com>
2019-12-08 13:27:25 +0900

    Add Models

---
+++ MVC/Models/Question.swift
@@ -0,0 +1,15 @@
+//
+//  Question.swift
+//  MVC
+//
+//  Created by victor on 08/12/2019.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ MVC/Models/QuestionGroup.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionGroup.swift
+//  MVC
+//
+//  Created by victor on 08/12/2019.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
