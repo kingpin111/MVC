0838d46116718437fb97dd59541de61154569485
victor <infragbe@gmail.com>
2019-12-08 14:10:34 +0900

    QuestionView 추가 및 Outlet 연결

---
+++ MVC/Views/QuestionView.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionView.swift
+//  MVC
+//
+//  Created by victor on 08/12/2019.
+//  Copyright © 2019 victor. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+}
